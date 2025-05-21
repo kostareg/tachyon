e6d1bfad9ae8840da9f48fe5a990fe021dea156e
kostareg <areg.kostanyan@gmail.com>
2025-05-21 09:27:35 -0400

    chore: add basic type ast nodes

--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -1,8 +1,50 @@
 #include <expected>
+#include <print>
 
 #include "ast/ast.hpp"
 
 namespace ast {
+
+struct PrintLiteral {
+    void operator()(const std::string &val) const { std::print("{}", val); }
+    void operator()(double val) const { std::print("{}", val); }
+    void operator()(bool val) const {
+        std::print("{}", val ? "true" : "false");
+    }
+    void operator()(std::monostate _) const { std::print("()"); }
+};
+
+struct Printer {
+    void operator()(const LiteralExpr &literal) const {
+        std::visit(PrintLiteral{}, literal.value);
+    }
+
+    void operator()(const BinaryOperatorExpr &binop) const {
+        std::visit(*this, binop.left->kind);
+        std::print(" {} ", op_to_str(binop.op));
+        std::visit(*this, binop.right->kind);
+    }
+
+    void operator()(const LetExpr &vdecl) const {
+        std::print("variable {} = ", vdecl.name);
+        std::visit(*this, vdecl.value->kind);
+    }
+
+    void operator()(const LetRefExpr &vref) const {
+        std::print("{}", vref.name);
+    }
+
+    void operator()(const ImportExpr &imp) const {
+        std::print("import {}", imp.name);
+    }
+
+    void operator()(const SequenceExpr &seq) const {
+        for (const Expr &e : seq.sequence) {
+            std::visit(*this, e.kind);
+        }
+    }
+};
+
 std::expected<vm::Proto, Error> generate_proto(ExprRefs) {
     return std::unexpected(Error(ErrorKind::ParseError, "A", 0, 0, 0, 0));
 }
--- src/ast/ast.hpp
+++ src/ast/ast.hpp
@@ -2,9 +2,12 @@
 
 #include <expected>
 #include <memory>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "error.hpp"
+#include "op.hpp"
 #include "vm/vm.hpp"
 
 /**
@@ -17,13 +20,81 @@ struct Expr;
 using ExprRef = std::unique_ptr<Expr>;
 using ExprRefs = std::vector<std::unique_ptr<Expr>>;
 
-// kinds of exprs
+/**
+ * @brief literal values, ie strings, numbers, booleans, unit.
+ */
+using LiteralValue = std::variant<double, std::string, bool, std::monostate>;
 
 struct LiteralExpr {
-    int myint;
+    LiteralValue value;
 };
 
-using ExprKind = std::variant<LiteralExpr>;
+/**
+ * @brief four basic concrete types.
+ */
+enum class BasicConcreteTypes {
+    Number,
+    String,
+    Boolean,
+    Unit,
+};
+
+// fwd-decl
+struct FunctionConcreteTypes;
+
+/**
+ * @brief the "Type" value, ie a reference to a concrete type or enum/struct.
+ */
+using Type =
+    std::variant<BasicConcreteTypes, FunctionConcreteTypes, std::string>;
+
+using MaybeType = std::optional<Type>;
+
+struct FunctionConcreteTypes {
+    std::unordered_map<std::string, Type> arguments;
+};
+
+/**
+ * @brief a function declaration expression, eg `fn(x) { x + 1 }`
+ *
+ * All functions are unnamed by default in Tachyon, so this structure only
+ * needs three values: the arguments, the return value, and the body. The
+ * arguments are an unordered map of the name and type. Since this is the weak
+ * side of the AST, types can be left blank to be inferred (std::nullopt).
+ */
+struct FnExpr {
+    std::unordered_map<std::string, MaybeType> arguments;
+    MaybeType returns;
+    ExprRef body;
+};
+
+/**
+ * @brief <expr> +, -, *, /, ^ <expr>
+ */
+struct BinaryOperatorExpr {
+    Op op;
+    ExprRef left;
+    ExprRef right;
+};
+
+struct LetExpr {
+    std::string name;
+    ExprRef value;
+};
+
+struct LetRefExpr {
+    std::string name;
+};
+
+struct ImportExpr {
+    std::string name;
+};
+
+struct SequenceExpr {
+    std::vector<Expr> sequence;
+};
+
+using ExprKind = std::variant<LiteralExpr, BinaryOperatorExpr, SequenceExpr>;
 
 /**
  * @brief Top-level expression type.
