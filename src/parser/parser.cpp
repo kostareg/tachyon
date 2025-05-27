7007d9b8f8577e089f37111e895800696dfd7a81
kostareg <areg.kostanyan@gmail.com>
2025-05-27 14:37:28 -0400

    feat: parse variable declarations

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -2,6 +2,7 @@ module;
 
 #include <expected>
 #include <memory>
+#include <optional>
 #include <string>
 #include <variant>
 
@@ -11,12 +12,51 @@ import lexer;
 using enum TokenType;
 
 namespace parser {
-std::expected<ast::Expr, Error> Parser::parse(int rbp) {
+std::expected<ast::Expr, Error> Parser::parse() {
+    std::vector<ast::Expr> stmts;
+    while (auto x = parse_stmt()) {
+        if (!x)
+            return std::unexpected(x.error());
+        if (!match(NLINE))
+            return std::unexpected(
+                Error(ErrorKind::ParseError, "expected new line", peek().span));
+        stmts.push_back(std::move(x.value()));
+    }
+
+    return Expr(SequenceExpr(std::move(stmts)));
+}
+
+std::expected<ast::Expr, Error> Parser::parse_stmt() {
+    if (match(IDENT) && match(EQ, 1)) {
+        auto l = advance();
+        if (!l)
+            return std::unexpected(l.error());
+        if (!std::holds_alternative<std::string>(l->value)) {
+            return std::unexpected(Error(ErrorKind::ParseError,
+                                         "variable name should be text ",
+                                         l->span));
+        }
+
+        auto _eq = advance();
+
+        auto r = parse_expr();
+        if (!r)
+            return std::unexpected(r.error());
+
+        return Expr(LetExpr(std::get<std::string>(l->value),
+                            std::make_unique<Expr>(std::move(r.value()))),
+                    l->span);
+    }
+
+    return parse_expr();
+}
+
+std::expected<ast::Expr, Error> Parser::parse_expr(int rbp) {
     auto t = advance();
     if (!t)
         return std::unexpected(t.error());
 
-    auto l = parse_nud(t.value());
+    auto l = parse_expr_nud(t.value());
     if (!l)
         return std::unexpected(l.error());
 
@@ -29,7 +69,7 @@ std::expected<ast::Expr, Error> Parser::parse(int rbp) {
         if (!t)
             return std::unexpected(t.error());
 
-        l = parse_led(t.value(), std::move(l.value()));
+        l = parse_expr_led(t.value(), std::move(l.value()));
         if (!l)
             return std::unexpected(l.error());
     }
@@ -37,7 +77,7 @@ std::expected<ast::Expr, Error> Parser::parse(int rbp) {
     return l;
 }
 
-std::expected<ast::Expr, Error> Parser::parse_nud(Token t) {
+std::expected<ast::Expr, Error> Parser::parse_expr_nud(Token t) {
     if (t.type == NUMBER)
         return Expr(LiteralExpr(LiteralValue(std::get<double>(t.value))),
                     t.span);
@@ -48,13 +88,15 @@ std::expected<ast::Expr, Error> Parser::parse_nud(Token t) {
         return Expr(LiteralExpr(LiteralValue(std::get<bool>(t.value))), t.span);
     else if (t.type == UNIT)
         return Expr(LiteralExpr(LiteralValue()), t.span);
+    else if (t.type == IDENT)
+        return Expr(LetRefExpr(std::get<std::string>(t.value)), t.span);
 
     return std::unexpected(
-        Error(ErrorKind::ParseError, "could not find literal", t.span));
+        Error(ErrorKind::ParseError, "could not parse expression", t.span));
 }
 
-std::expected<ast::Expr, Error> Parser::parse_led(Token t, Expr l) {
-    auto r = parse(get_lbp(t.type));
+std::expected<ast::Expr, Error> Parser::parse_expr_led(Token t, Expr l) {
+    auto r = parse_expr(get_lbp(t.type));
     if (!r)
         return std::unexpected(r.error());
     return Expr(BinaryOperatorExpr(
@@ -68,5 +110,19 @@ std::expected<Token, Error> Parser::advance() {
     if (i + 1 >= ts.size())
         return std::unexpected(Error(ErrorKind::ParseError, "", ts[i].span));
     return ts[i++];
+}
+
+bool Parser::match(TokenType tt) {
+    if (peek().type == tt) {
+        return true;
+    }
+    return false;
+}
+
+bool Parser::match(TokenType tt, size_t x) {
+    if (ts[i + x].type == tt) {
+        return true;
+    }
+    return false;
 }
 } // namespace parser
--- src/parser/parser.cppm
+++ src/parser/parser.cppm
@@ -1,6 +1,7 @@
 module;
 
 #include <expected>
+#include <optional>
 
 export module parser;
 import ast;
@@ -19,20 +20,33 @@ export class Parser {
   public:
     explicit Parser(Tokens ts) : ts(ts) {}
 
-    std::expected<ast::Expr, Error> parse(int rbp = 0);
+    std::expected<ast::Expr, Error> parse();
+
+    std::expected<ast::Expr, Error> parse_stmt();
+
+    std::expected<ast::Expr, Error> parse_expr(int rbp = 0);
 
     /**
      * @brief null denotation (literals, unary operators)
      */
-    std::expected<ast::Expr, Error> parse_nud(Token t);
+    std::expected<ast::Expr, Error> parse_expr_nud(Token t);
 
     /**
      * @brief left denotation (binary operators with infix positions)
      */
-    std::expected<ast::Expr, Error> parse_led(Token t, Expr l);
+    std::expected<ast::Expr, Error> parse_expr_led(Token t, Expr l);
 
     Token peek();
     std::expected<Token, Error> advance();
+
+    /**
+     * @brief matches token type to current token, advances if possible
+     *
+     * @param tt token type
+     * @return matches?
+     */
+    bool match(TokenType tt);
+    bool match(TokenType tt, size_t x);
 };
 
 /**
